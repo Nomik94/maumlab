ff3c454569a989e20dd1bf52d5c2f1caa1169fbd
Nomik94 <kill0374@naver.com>
2023-11-15 20:26:36 +0900

    chore : typeorm config setting

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,19 @@
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (configService: ConfigService) => {
+    return {
+      type: 'postgres',
+      host: configService.get('DB_HOST'),
+      port: 5432,
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_DATABASE'),
+      entities: [__dirname + '../**/*.entity.{js,ts}'],
+      synchronize: true,
+    };
+  },
+};
