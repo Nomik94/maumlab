4646c95da0787f662ef0c99c4f3a7eecca910dbf
Nomik94 <kill0374@naver.com>
2023-11-17 23:46:52 +0900

    fix : user 삭제

--- src/modules/user/interface/user.repository.interface.ts
+++
@@ -1,5 +0,0 @@
-import { BaseInterfaceRepository } from '@/common/repository/base.interface.repository';
-import { User } from '@/modules/user/entity/user.entity';
-
-export interface UserRepositoryInterface
-  extends BaseInterfaceRepository<User> {}
