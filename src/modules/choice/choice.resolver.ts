b7b6e6062ec8f2539c50bb3ebe54ca0d20ae2ada
Nomik94 <kill0374@naver.com>
2023-11-18 22:48:18 +0900

    feature : choice 기능 추가

--- src/modules/choice/choice.resolver.ts
+++ src/modules/choice/choice.resolver.ts
@@ -1,7 +1,46 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { ChoiceService } from '@/modules/choice/choice.service';
+import { Choice } from '@/modules/choice/entity/choice.entity';
+import { CreateChoiceInput } from '@/modules/choice/input/create-choice.input';
+import { UpdateChoiceInput } from '@/modules/choice/input/update-choice.input';
 
 @Resolver()
 export class ChoiceResolver {
   constructor(private readonly choiceService: ChoiceService) {}
+  @Mutation(() => Choice)
+  createChoice(@Args('input') input: CreateChoiceInput): Promise<Choice> {
+    return this.choiceService.createChoice(input);
+  }
+
+  @Query(() => [Choice])
+  getAllChoice(): Promise<Choice[]> {
+    return this.choiceService.findAllChoice();
+  }
+
+  @Query(() => Choice)
+  getByIdChoice(
+    @Args({ name: 'id', type: () => Int }) id: number,
+  ): Promise<Choice> {
+    return this.choiceService.findOneByIdChoice(id);
+  }
+
+  @Mutation(() => String)
+  async updateChoice(
+    @Args({ name: 'id', type: () => Int }) id: number,
+    @Args('input')
+    input: UpdateChoiceInput,
+  ): Promise<string> {
+    await this.choiceService.updateChoice(id, input);
+
+    return `Success Update Choice id:${id}`;
+  }
+
+  @Mutation(() => String)
+  async deleteChoice(
+    @Args({ name: 'id', type: () => Int }) id: number,
+  ): Promise<string> {
+    await this.choiceService.removeChoice(id);
+
+    return `Success Delete Choice id:${id}`;
+  }
 }
--- src/modules/choice/choice.service.ts
+++ src/modules/choice/choice.service.ts
@@ -1,5 +1,15 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { ChoiceRepositoryInterface } from '@/modules/choice/interface/choice.repository.interface';
+import { CreateChoiceInput } from '@/modules/choice/input/create-choice.input';
+import { Choice } from '@/modules/choice/entity/choice.entity';
+import { UpdateChoiceInput } from '@/modules/choice/input/update-choice.input';
+import { UpdateResult } from 'typeorm';
+import { createDate } from '@/common/date';
 
 @Injectable()
 export class ChoiceService {
@@ -7,4 +17,53 @@ export class ChoiceService {
     @Inject('ChoiceRepositoryInterface')
     private readonly choiceRepository: ChoiceRepositoryInterface,
   ) {}
+
+  async createChoice(input: CreateChoiceInput): Promise<Choice> {
+    const { questionId, ...choice } = input;
+    try {
+      const choiceEntity: Choice = this.choiceRepository.create({
+        ...choice,
+        question: { id: questionId },
+      });
+      return await this.choiceRepository.save(choiceEntity);
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
+  }
+
+  async findAllChoice(): Promise<Choice[]> {
+    try {
+      return await this.choiceRepository.findAll();
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
+  }
+
+  async findOneByIdChoice(id: number): Promise<Choice> {
+    const choice: Choice = await this.choiceRepository.findOneById(id);
+    if (!choice) {
+      throw new NotFoundException(`Choice with id:${id} not found`);
+    }
+    return choice;
+  }
+
+  async updateChoice(
+    id: number,
+    data: UpdateChoiceInput,
+  ): Promise<UpdateResult> {
+    try {
+      return await this.choiceRepository.update(id, data);
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
+  }
+
+  async removeChoice(id: number): Promise<UpdateResult> {
+    try {
+      const deletedAt: Date = createDate();
+      return await this.choiceRepository.update(id, { deletedAt });
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
+  }
 }
