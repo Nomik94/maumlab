582230483506d0e5b8d2dcc8073fa61b6b18f07c
Nomik94 <kill0374@naver.com>
2023-11-20 20:42:28 +0900

    feature : 완료된 설문지 기능 추가

--- src/modules/completed-survey/completed-survey.resolver.ts
+++ src/modules/completed-survey/completed-survey.resolver.ts
@@ -1,7 +1,7 @@
 import { CompletedSurveyService } from '@/modules/completed-survey/completed-survey.service';
 import { CompletedSurvey } from '@/modules/completed-survey/entity/completed-survey.entity';
 import { CreateCompletedSurveyInput } from '@/modules/completed-survey/input/create-completed-survey.input';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class CompletedSurveyResolver {
@@ -11,7 +11,26 @@ export class CompletedSurveyResolver {
   @Mutation(() => CompletedSurvey)
   async createCompletedSurvey(
     @Args('input') input: CreateCompletedSurveyInput,
-  ) {
+  ): Promise<CompletedSurvey> {
     return this.completedSurveyService.createCompletedSurvey(input);
+  }
+
+  @Query(() => [CompletedSurvey])
+  async getAllCompletedSurvey(): Promise<CompletedSurvey[]> {
+    return this.completedSurveyService.findAllCompletedSurvey();
+  }
+
+  @Query(() => CompletedSurvey)
+  async getByIdCompletedSurvey(
+    @Args({ name: 'id', type: () => Int }) id: number,
+  ): Promise<CompletedSurvey> {
+    return this.completedSurveyService.findOneCompletedSurvey(id);
+  }
+
+  @Query(() => CompletedSurvey)
+  async getRelationResponseDetail(
+    @Args({ name: 'id', type: () => Int }) id: number,
+  ): Promise<CompletedSurvey> {
+    return this.completedSurveyService.findOneAndRelationResponse(id);
   }
 }
